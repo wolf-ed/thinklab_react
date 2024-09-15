2f7a2d302749e447ae287dfd77364ca9076c8835
unknown <wolf7ed@gmail.com>
2024-09-15 10:10:59 +0200

    improve top nav bar styles for mobile

--- src/navigation/TopNavBar/TopNavBar.tsx
+++ src/navigation/TopNavBar/TopNavBar.tsx
@@ -1,86 +1,22 @@
-import React from 'react';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
 //MUI
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 
 // LOCAL
 import { LeftDrawer } from '../LeftDrawer/LeftDrawer';
 import { setIsLetDrawerOpen } from '../../store/user/userSlice';
 import { StyledMainNavigation } from './TopNavBar.styles';
-import { ROUTES_ENUM } from '../const';
 import { App_Colors } from '../../styles/globalStyles';
 import { AppNameStyled } from './TopNavBar.styles';
-import { useAuth } from '../../hooks/useAuth/useAuth';
 import { TopRightOptions } from './components/TopRightOptions/TopRightOptions';
-import { ENV_IS_PROD } from '../../envConsts';
-// import { TopRightOptions } from './components/TopRightOptions/TopRightOptions';
-// import { userSelectors } from '../../store/user/userSelectors';
 
 export const TopNavBar = () => {
-  const auth = useAuth();
   const dispatch = useDispatch();
-  // const isAuth = useSelector(userSelectors.getIsAuth);
-  const navigate = useNavigate();
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-
-  const userAccountIconButtonMenu = 'primary-search-account-menu';
-  /**
-   * its hidden, but needs to be there so when we click the account button we see the options
-   */
-  const personIconClickMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={userAccountIconButtonMenu}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem
-        onClick={() => {
-          navigate(ROUTES_ENUM.USER_ACCOUNT);
-          handleMenuClose();
-        }}
-      >
-        My account
-      </MenuItem>
-      <MenuItem
-        onClick={() => {
-          auth.logOutUser();
-          navigate(ROUTES_ENUM.LOG_IN);
-          handleMenuClose();
-        }}
-      >
-        Logout
-      </MenuItem>
-    </Menu>
-  );
 
   const threeBarsMenuIcon = (
     <IconButton
@@ -99,19 +35,6 @@ export const TopNavBar = () => {
 
   const appNameTitleComponent = <AppNameStyled>ThinkLab</AppNameStyled>;
 
-  const accountIconButton = (
-    <IconButton
-      size="large"
-      edge="end"
-      aria-label="account of current user"
-      aria-controls={userAccountIconButtonMenu}
-      aria-haspopup="true"
-      onClick={handleProfileMenuOpen}
-      color="inherit"
-    >
-      <AccountCircle />
-    </IconButton>
-  );
   const boxThatExpandsToSeparateLeftAndRightSide = <Box sx={{ flexGrow: 1 }} />;
 
   const leftSideButtons = (
@@ -133,11 +56,9 @@ export const TopNavBar = () => {
           {leftSideButtons}
           {boxThatExpandsToSeparateLeftAndRightSide}
           {<TopRightOptions />}
-          {!ENV_IS_PROD && accountIconButton}
         </Toolbar>
         <LeftDrawer />
       </AppBar>
-      {personIconClickMenu}
     </StyledMainNavigation>
   );
 };
--- src/navigation/TopNavBar/components/TopRightOptions/TopRightOptions.tsx
+++ src/navigation/TopNavBar/components/TopRightOptions/TopRightOptions.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import styled from '@emotion/styled';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MenuItem from '@mui/material/MenuItem';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 // LOCAL
 import { getIsAuth } from '../../../../store/user/userSelectors';
 import { ROUTES_ENUM } from '../../../const';
-
+import { App_Colors } from '../../../../styles/globalStyles';
+import { useAuth } from '../../../../hooks/useAuth/useAuth';
 const StyledOptionsBox = styled(Box)({
   display: 'flex',
   gap: '1rem',
@@ -14,26 +22,127 @@ const StyledOptionsBox = styled(Box)({
 
 export const TopRightOptions = () => {
   const isAuth = getIsAuth();
+  const auth = useAuth();
   const navigate = useNavigate();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isMenuOpen = Boolean(anchorEl);
 
   const handleLogin = () => {
     navigate(ROUTES_ENUM.LOG_IN);
+    handleClose();
   };
 
   const handleSignUp = () => {
     navigate(ROUTES_ENUM.SIGN_UP);
+    handleClose();
   };
 
   const handlePrivacyPolicy = () => {
     navigate(ROUTES_ENUM.PRIVACY_POLICY);
+    handleClose();
   };
 
-  if (isAuth) {
-    return <StyledOptionsBox>{'User is logged in'}</StyledOptionsBox>;
-  }
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-  return (
-    <StyledOptionsBox>
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const userAccountIconButtonMenu = 'primary-search-account-menu';
+
+  const accountIconButton = (
+    <IconButton
+      size="large"
+      edge="end"
+      aria-label="account of current user"
+      aria-controls={userAccountIconButtonMenu}
+      aria-haspopup="true"
+      onClick={handleProfileMenuOpen}
+      color="inherit"
+    >
+      <AccountCircle />
+    </IconButton>
+  );
+  const personIconClickMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={userAccountIconButtonMenu}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem
+        onClick={() => {
+          navigate(ROUTES_ENUM.USER_ACCOUNT);
+          handleMenuClose();
+        }}
+      >
+        My account
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          handlePrivacyPolicy();
+          handleMenuClose();
+        }}
+      >
+        Privacy Policy
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          auth.logOutUser();
+          navigate(ROUTES_ENUM.LOG_IN);
+          handleMenuClose();
+        }}
+      >
+        Logout
+      </MenuItem>
+    </Menu>
+  );
+
+  const notAuthOptions = isMobile ? (
+    <>
+      <IconButton
+        aria-label="more"
+        aria-controls="long-menu"
+        aria-haspopup="true"
+        sx={{ color: App_Colors.contrastColor }}
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {isAuth ?? <MenuItem onClick={handleLogin}>Log In</MenuItem>}
+        {isAuth ?? <MenuItem onClick={handleSignUp}>Sign Up</MenuItem>}
+        <MenuItem onClick={handlePrivacyPolicy}>Privacy Policy</MenuItem>
+      </Menu>
+    </>
+  ) : (
+    <>
       <Button color="inherit" onClick={handleLogin}>
         Log In
       </Button>
@@ -43,6 +152,13 @@ export const TopRightOptions = () => {
       <Button color="inherit" onClick={handlePrivacyPolicy} variant="outlined">
         Privacy Policy
       </Button>
+    </>
+  );
+
+  return (
+    <StyledOptionsBox>
+      {isAuth ? accountIconButton : notAuthOptions}
+      {personIconClickMenu}
     </StyledOptionsBox>
   );
 };
