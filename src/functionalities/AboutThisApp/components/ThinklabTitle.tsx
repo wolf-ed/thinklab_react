212bd84edc30ef76ccaa3d6b513625a32aa286fe
unknown <wolf7ed@gmail.com>
2024-09-18 07:40:18 +0200

    improve ThinklabTitle

--- src/functionalities/AboutThisApp/components/ThinklabTitle.tsx
+++ src/functionalities/AboutThisApp/components/ThinklabTitle.tsx
@@ -1,18 +1,30 @@
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export const ThinklabTitle = () => {
+  const isMobile = useMediaQuery('(max-width: 600px)');
+
   return (
-    <Paper elevation={3} sx={{ padding: '2rem', marginBottom: '3rem' }}>
+    <Paper
+      elevation={3}
+      sx={{ padding: isMobile ? '1rem' : '2rem', marginBottom: '3rem' }}
+    >
       <Typography
         variant="h2"
         gutterBottom
         align="center"
-        sx={{ fontWeight: 'bold' }}
+        sx={{ fontWeight: 'bold', fontSize: isMobile ? '3rem' : '3rem' }}
       >
-        About ThinkLab
+        ThinkLab
       </Typography>
-      <Typography variant="h3" paragraph align="center" color="textSecondary">
+      <Typography
+        variant="h3"
+        paragraph
+        align="center"
+        color="textSecondary"
+        sx={{ fontWeight: 'bold', fontSize: isMobile ? '2rem' : '3rem' }}
+      >
         <strong>Author:</strong> Alvaro E. BL
       </Typography>
     </Paper>
